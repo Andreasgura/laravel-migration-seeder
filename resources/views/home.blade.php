4a81065aebab4c7360716113f8571d9cce789a7d
Andrea Sgura <andre-56@hotmail.it>
2024-05-22 17:15:42 +0200

    printed all trains

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,6 +5,23 @@
 @section('content')
 <main>
     <h1>Home page</h1>
+    <div class="container">
+        <div class="row">
+            @foreach ($trains as $train)
+            <div class="col-3">
+                <ul>
+                    <li>{{ $train->company }}</li>
+                    <li>Stazione di partenza: {{ $train->departure_station }}</li>
+                    <li>Stazione di arrivo: {{ $train->arrival_station }}</li>
+                    <li>Orario di partenza: {{ $train->departure_time }}</li>
+                    <li>Orario di arrivo: {{ $train->arrival_time }}</li>
+                </ul>
+            </div>
+            @endforeach
+        </div>
+    </div>
+
+
 </main>
 
 @endsection
